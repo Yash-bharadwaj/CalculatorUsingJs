07cbd0d94c82a870bbf0f8d2155e69f35fbd81a2
Yashwanth Bharadwaj <86095452+Yash-bharadwaj@users.noreply.github.com>
2023-06-18 00:19:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,10 +20,7 @@
         else{
             let answer = eval(screen.value);
             screen.value = answer;
-            document.body.style.backgroundImage = url("https://giphy.com/gifs/Trakto--background-mograph-trakto-7AtHoQ9XWbpwLRxs0t");
-            document.querySelector('.buttons').style.background = '#f0f8ff55';
-            document.getElementById('title').style = 'color:#f3e6d0';
-            document.getElementById('title').style = 'font-size:';
+         
         }
     })
 
